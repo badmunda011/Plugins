ad7be7c6717809bd1d68d4bfcb6439095988ac2f
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-07-15 11:33:01 +0530

    Update __init__.py

--- TelethonPbx/DB/__init__.py
+++ TelethonPbx/DB/__init__.py
@@ -1,11 +1,12 @@
 from PbxConfig import Config
+from PbxConfig import *
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(Config.DATABASE_URL)
+    engine = create_engine(Config.ELEPHANTSQL_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
@@ -17,6 +18,6 @@ try:
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
     print(
-        "DATABASE_URL is not configured. Features depending on the database might have issues."
+        "ELEPHANTSQL_URL is not configured. Features depending on the database might have issues."
     )
     print(str(e))
