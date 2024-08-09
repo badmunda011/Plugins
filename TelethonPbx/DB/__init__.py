3c4392c0696e5384cbab53373a94e61a11f8431a
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-08-09 10:07:51 +0530

    Update __init__.py

--- TelethonPbx/DB/__init__.py
+++ TelethonPbx/DB/__init__.py
@@ -1,12 +1,11 @@
 from PbxConfig import Config
-from PbxConfig import *
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(postgres://u94bvurcphj34k:p88947d7eef18ddde09311863e892248be33064faa453b5a7792270e9ab27c7c5@c4jpa9p9eiug0j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d5phr18iooe2k0)
+    engine = create_engine(Config.DATABASE_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
